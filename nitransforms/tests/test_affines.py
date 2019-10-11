f4e6f9562ed339063e0970df1ec9112f6642a5df
oesteban <code@oscaresteban.es>
2019-10-11 16:35:58 -0700

    fix: obliquity test was missing imports

--- nitransforms/tests/test_affines.py
+++ nitransforms/tests/test_affines.py
@@ -1,7 +1,6 @@
 import numpy as np
 from nibabel.affines import from_matvec
 from nibabel.eulerangles import euler2mat
-from nose.tools import assert_equal, assert_raises
 from ..patched import obliquity
 
 def test_obliquity():
@@ -11,6 +10,6 @@ def test_obliquity():
     aligned[:-1, -1] = [-10, -10, -7]
     R = from_matvec(euler2mat(x=0.09, y=0.001, z=0.001), [0.0, 0.0, 0.0])
     oblique = R.dot(aligned)
-    assert_almost_equal(obliquity(aligned), [0.0, 0.0, 0.0])
-    assert_almost_equal(obliquity(oblique) * 180 / pi,
-                        [0.0810285, 5.1569949, 5.1569376])
+    np.testing.assert_almost_equal(obliquity(aligned), [0.0, 0.0, 0.0])
+    np.testing.assert_almost_equal(obliquity(oblique) * 180 / pi,
+                                   [0.0810285, 5.1569949, 5.1569376])
