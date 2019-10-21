77fdbc991ec28dd54c648be8e5df520eea4f2de3
mathiasg <mathiasg@mit.edu>
2019-10-21 18:04:18 -0400

    enh: support lta loading

--- nitransforms/tests/utils.py
+++ nitransforms/tests/utils.py
@@ -11,9 +11,15 @@ def assert_affines_by_filename(affine1, affine2):
     affine2 = Path(affine2)
     assert affine1.suffix == affine2.suffix, 'affines of different type'
 
-    if affine1.suffix.endswith('.tfm'):  # An ITK transform
-        xfm1 = nbl.load(str(affine1), fmt='itk')
-        xfm2 = nbl.load(str(affine2), fmt='itk')
+    ext_to_fmt = {
+        '.tfm': 'itk',  # An ITK transform
+        '.lta': 'fs',  # FreeSurfer LTA
+    }
+
+    ext = affine1.suffix[-4:]
+    if ext in ext_to_fmt:
+        xfm1 = nbl.load(str(affine1), fmt=ext_to_fmt[ext])
+        xfm2 = nbl.load(str(affine2), fmt=ext_to_fmt[ext])
         assert xfm1 == xfm2
     else:
         xfm1 = np.loadtxt(str(affine1))
