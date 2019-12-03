8bedfa42ee4cb936e5fa60f3c7f902eae8aa93a3
mathiasg <mathiasg@stanford.edu>
2019-12-03 16:35:24 -0500

    enh: improve argument documentation, some basic validation

--- nitransforms/cli.py
+++ nitransforms/cli.py
@@ -1,30 +1,49 @@
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 from textwrap import dedent
-
-import nibabel as nb
+import sys
 
 from .linear import load as linload
 from .nonlinear import load as nlinload
 
-def cli_apply(**kwargs):
+
+def cli_apply(pargs):
     """
     Apply a transformation to an image, resampling on the reference
 
-    Usage:
+    Sample usage:
 
-        $ nt apply moving.nii.gz xform.fsl --reference reference.nii.gz
+        $ nt apply xform.fsl moving.nii.gz --ref reference.nii.gz
+
+        $ nt apply warp.nii.gz moving.nii.gz --fmt afni --nonlinear
+
     """
-    try:
-        xfm = linload(xform)
-    except:
-        xfm = nlinload(xform)
+    fmt = pargs.fmt or pargs.transform.split('.')[-1]
+    if fmt == 'tfm':
+        fmt = 'itk'
+    elif fmt == 'lta':
+        fmt = 'fs'
 
-    xfm.apply(moving, **kwargs)
+    if fmt not in ('fs', 'itk', 'fsl', 'afni'):
+        raise RuntimeError(
+            "Cannot determine transformation format, manually set format with the `--fmt` flag"
+        )
+
+    if pargs.nonlinear:
+        xfm = nlinload(pargs.transform, fmt=fmt, reference=pargs.ref)
+    else:
+        xfm = linload(pargs.transform, fmt=fmt)
+
+    xfm.apply(
+        pargs.moving,
+        order=pargs.order,
+        mode=pargs.mode,
+        cval=pargs.cval,
+        prefilter=pargs.prefilter
+    )
 
 
 def get_parser():
-    desc = dedent(
-        """
+    desc = dedent("""
         NiTransforms command-line utility.
 
         Commands:
@@ -32,13 +51,13 @@ def get_parser():
             apply       Apply a transformation to an image
 
         For command specific information, use 'nt <command> -h'.
-        """
-    )
+    """)
 
     parser = ArgumentParser(
         description=desc, formatter_class=RawDescriptionHelpFormatter
     )
     subparsers = parser.add_subparsers(dest='command')
+
     def _add_subparser(name, description):
         subp = subparsers.add_parser(
             name,
@@ -52,18 +71,42 @@ def get_parser():
     applyp.add_argument(
         'moving', help='The image containing the data to be resampled'
     )
-    applyp.add_argument('reference', help='The reference space to resample onto')
-    applyp.add_argument('--order', help='The order of the spline transformation')
+    applyp.add_argument('--ref', help='The reference space to resample onto')
     applyp.add_argument(
-        '--mode', 
-        help='Determines how the input image is extended when the resampling overflows a border'
+        '--fmt',
+        choices=('itk', 'fsl', 'afni', 'fs'),
+        help='Format of transformation. If no option is passed, nitransforms will '
+        'estimate based on the transformation file extension.'
     )
-    applyp.add_argument('--cval', help='Constant used when using "constant" mode')
     applyp.add_argument(
-        '--prefilter', 
-        action='store_true', 
+        '--nonlinear', action='store_true', help='Transformation is nonlinear (default: False)'
+    )
+    applykwargs = applyp.add_argument_group('Apply customization')
+    applykwargs.add_argument(
+        '--order',
+        type=int,
+        default=3,
+        choices=range(6),
+        help='The order of the spline transformation (default: 3)'
+    )
+    applykwargs.add_argument(
+        '--mode',
+        choices=('constant', 'reflect', 'nearest', 'mirror', 'wrap'),
+        default='constant',
+        help='Determines how the input image is extended when the resampling overflows a border '
+        '(default: constant)'
+    )
+    applykwargs.add_argument(
+        '--cval',
+        type=float,
+        default=0.0,
+        help='Constant used when using "constant" mode (default: 0.0)'
+    )
+    applykwargs.add_argument(
+        '--prefilter',
+        action='store_false',
         help="Determines if the image's data array is prefiltered with a spline filter before "
-             "interpolation"
+        "interpolation (default: True)"
     )
     return parser
 
@@ -73,7 +116,7 @@ def main(pargs=None):
     pargs = parser.parse_args(pargs)
     if pargs.command is None:
         parser.print_help()
-        return
+        sys.exit(1)
 
     if pargs.command == 'apply':
-        cli_apply(**vars(pargs))
+        cli_apply(pargs)
