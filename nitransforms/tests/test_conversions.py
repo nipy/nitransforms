950b5fb92781176c1b4bb7cfff6c4cc7922fe7de
oesteban <code@oscaresteban.es>
2020-03-19 18:43:07 -0700

    ENH: Beef-up LTA tests with one equivalent to ``mri_concatenate_lta``
    This PR adds more LTA and corresponding ITK conversions. The ITK
    conversions have been calculated with ``lta_convert``. The original
    NIfTI files have also been added to this PR for the implementation of
    future tests.
    
    Files have been named with some sort of BIDS derived nomenclature. Some
    provenance has been recorded as initial comments of the LTA files.

--- nitransforms/tests/test_conversions.py
+++ nitransforms/tests/test_conversions.py
@@ -2,20 +2,32 @@
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 """Conversions between formats."""
 import numpy as np
+import pytest
 from .. import linear as _l
 
 
-def test_conversions0(data_path):
+@pytest.mark.parametrize('filename', [
+    "from-fsnative_to-bold_mode-image",
+    "from-fsnative_to-scanner_mode-image",
+    "from-scanner_to-bold_mode-image",
+    "from-scanner_to-fsnative_mode-image",
+])
+def test_lta2itk_conversions(data_path, filename):
     """Check conversions between formats."""
-    lta = _l.load(data_path / "regressions" / "robust_register.lta", fmt="lta")
-    itk = _l.load(data_path / "regressions" / "robust_register.tfm", fmt="itk")
-
+    lta = _l.load(data_path / "regressions" / ".".join((filename, "lta")),
+                  fmt="lta")
+    itk = _l.load(data_path / "regressions" / ".".join((filename, "tfm")),
+                  fmt="itk")
     assert np.allclose(lta.matrix, itk.matrix)
 
 
-def test_conversions1(data_path):
-    """Check conversions between formats."""
-    lta = _l.load(data_path / "regressions" / "bbregister.lta", fmt="lta")
-    itk = _l.load(data_path / "regressions" / "bbregister.tfm", fmt="itk")
+def test_concatenation(data_path):
+    """Check replacement to lta_concat."""
+    lta0 = _l.load(data_path / "regressions" / "from-scanner_to-fsnative_mode-image.lta",
+                   fmt="lta")
+    lta1 = _l.load(data_path / "regressions" / "from-fsnative_to-bold_mode-image.lta", fmt="lta")
 
-    assert np.allclose(lta.matrix, itk.matrix)
+    lta_combined = _l.load(data_path / "regressions" / "from-scanner_to-bold_mode-image.lta",
+                           fmt="lta")
+
+    assert np.allclose(lta1.matrix.dot(lta0.matrix), lta_combined.matrix)
