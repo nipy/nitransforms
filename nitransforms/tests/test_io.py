2c62fd9f0322e7b483c554838018eaac072f977c
Oscar Esteban <code@oscaresteban.es>
2025-07-25 20:10:40 +0200

    rf: move tests to more appropriate file

--- nitransforms/tests/test_io.py
+++ nitransforms/tests/test_io.py
@@ -23,7 +23,6 @@ from nitransforms.io import (
     fsl,
     lta as fs,
     itk,
-    x5,
 )
 from nitransforms.io.lta import (
     VolumeGeometry as VG,
@@ -765,52 +764,6 @@ def test_itk_displacements(tmp_path, get_testdata, image_orientation, field_is_r
     np.testing.assert_allclose(itk_nit_nii.dataobj.transpose(2, 1, 0, 3)[..., None, :], nit_nii.dataobj)
     np.testing.assert_allclose(itk_nit_nii.affine, ref_affine)
     np.testing.assert_allclose(LPS @ itk_nit_nii.affine, nit_nii.affine)
-
-
-@pytest.mark.parametrize("is_deltas", [True, False])
-def test_densefield_x5_roundtrip(tmp_path, is_deltas):
-    """Ensure dense field transforms roundtrip via X5."""
-    ref = nb.Nifti1Image(np.zeros((2, 2, 2), dtype="uint8"), np.eye(4))
-    disp = nb.Nifti1Image(np.random.rand(2, 2, 2, 3).astype("float32"), np.eye(4))
-
-    xfm = DenseFieldTransform(disp, is_deltas=is_deltas, reference=ref)
-
-    node = xfm.to_x5(metadata={"GeneratedBy": "pytest"})
-    assert node.type == "nonlinear"
-    assert node.subtype == "densefield"
-    assert node.representation == "displacements" if is_deltas else "deformations"
-    assert node.domain.size == ref.shape
-    assert node.metadata["GeneratedBy"] == "pytest"
-
-    fname = tmp_path / "test.x5"
-    x5.to_filename(fname, [node])
-
-    xfm2 = DenseFieldTransform.from_filename(fname, fmt="X5")
-
-    assert xfm2.reference.shape == ref.shape
-    assert np.allclose(xfm2.reference.affine, ref.affine)
-    assert xfm == xfm2
-
-
-def test_bspline_to_x5(tmp_path):
-    """Check BSpline transforms export to X5."""
-    coeff = nb.Nifti1Image(np.zeros((2, 2, 2, 3), dtype="float32"), np.eye(4))
-    ref = nb.Nifti1Image(np.zeros((2, 2, 2), dtype="uint8"), np.eye(4))
-
-    xfm = BSplineFieldTransform(coeff, reference=ref)
-    node = xfm.to_x5(metadata={"tool": "pytest"})
-    assert node.type == "nonlinear"
-    assert node.subtype == "bspline"
-    assert node.representation == "coefficients"
-    assert node.metadata["tool"] == "pytest"
-
-    fname = tmp_path / "bspline.x5"
-    x5.to_filename(fname, [node])
-
-    xfm2 = BSplineFieldTransform.from_filename(fname, fmt="X5")
-    assert np.allclose(xfm._coeffs, xfm2._coeffs)
-    assert xfm2.reference.shape == ref.shape
-    assert np.allclose(xfm2.reference.affine, ref.affine)
 
 
 # Added tests for h5 orientation bug
